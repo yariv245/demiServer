f98f2b2dd70a3cdbb7deb6fcceab5fffd48077be
Yariv Menachem <yariv220895@gmail.com>
2024-01-29 21:38:45 +0200

    handle global exception -> Exception.class

--- src/main/java/com/templateServer/demi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/templateServer/demi/exception/GlobalExceptionHandler.java
@@ -12,6 +12,19 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                  WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistException(CustomerAlreadyExistsException exception,
                                                                                 WebRequest webRequest) {
