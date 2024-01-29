eabfce61d5a173f64e3ceb2b4dc4e0f664a2d1fa
Yariv Menachem <yariv220895@gmail.com>
2024-01-29 21:10:38 +0200

    Initial commit

---
+++ src/main/java/com/templateServer/demi/DemiServerApplication.java
@@ -0,0 +1,13 @@
+package com.templateServer.demi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/templateServer/demi/DemiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.templateServer.demi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
