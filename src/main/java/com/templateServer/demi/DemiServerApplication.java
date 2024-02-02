c70e7eb9a27e03527b4ec2a8c9b4e20d41fd4b16
Yariv Menachem <yariv220895@gmail.com>
2024-02-02 17:02:30 +0200

    added swagger

--- src/main/java/com/templateServer/demi/DemiServerApplication.java
+++ src/main/java/com/templateServer/demi/DemiServerApplication.java
@@ -1,11 +1,19 @@
 package com.templateServer.demi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info (
+                title = "Demi server test ",
+                description = "This service created using Eazybytes course"
+        )
+)
 public class DemiServerApplication {
 
     public static void main(String[] args) {
